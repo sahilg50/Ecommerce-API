fbab432bd07a11a211d57910cb512c436bfce3af
Sahil <56653026+sahilg50@users.noreply.github.com>
2021-05-23 13:01:30 +0530

    CheckPoint 3: Done with the cart Items
    Co-authored-by: arun-upreti <arun-upreti@users.noreply.github.com>

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const cors = require('cors');
+const { urlencoded } = require('body-parser');
 const app = express();
 
 let user = {};
-app.use(bodyParser.json());
+var cart_Id = '';
+app.use(bodyParser.json({ limit: '50mb', extended: true }));
 
 app.use(cors());
 
@@ -28,6 +30,8 @@ connection.connect((err) => {
 //Query to Store user in the databse
 app.post('/', (req, res) => {
 	user = req.body;
+	cart_Id = 'cart'.concat(user.userId);
+	console.log(cart_Id);
 	res.json('UserVerified');
 
 	const userDetails = {
@@ -110,23 +114,105 @@ app.get('/total_categories', (req, res) => {
 	});
 });
 
-//Add the Items to the cart
-app.post('/add_item_to_cart', (req, res) => {
-	const cart_Id = 'cart'.concat(user.userId);
-	const product_Id = req.body.id;
-	console.log(cart_Id);
-	console.log(product_Id);
-
+//Retrieve cart items when user logins
+app.get('/retrieve_cart_items', (req, res) => {
 	var sql =
-		"use ecommerce;SET SQL_SAFE_UPDATES = 0;set @productId='3';set @cartId = '1';create table temp (productId varchar(20),cartId varchar(20));Insert into temp VALUES(@productId, @cartId);UPDATE cart set Quantity =Quantity+1 where productId = @productId and cartId=@cartId;insert into cart(productId, CartId, Quantity)select productId, cartId, 1 as'Quantity' from temp where productId not in (select productId from cart where cartId=@cartId);Drop table temp;";
+		'SELECT cart.CartId,cart.Quantity,cart.productid, product.productImage,product.productPrice,product.productName FROM cart left join product on cart.productId=product.productId where cartId=?';
+	connection.query(sql, cart_Id, (err, rows) => {
+		if (err) {
+			console.log('Cart Items cannot be sent to the frontend');
+		} else {
+			console.log(rows);
+			const test = JSON.stringify(rows);
+			res.json(JSON.parse(test));
+		}
+	});
+});
 
-	const vars = [product_Id, cart_Id];
-	connection.query(sql, (err, rows) => {
+// //Retrieve item when user logins
+// app.post('/retrieve_item', (req, res) => {
+// 	var sql =
+// 		'';
+// 	connection.query(sql, cart_Id, (err, rows) => {
+// 		if (err) {
+// 			console.log('Cart Items cannot be sent to the frontend');
+// 		} else {
+// 			console.log(rows);
+// 			const test = JSON.stringify(rows);
+// 			res.json(JSON.parse(test));
+// 		}
+// 	});
+// });
+
+//Insert a new item to the cart
+app.post('/insert_item_to_cart', (req, res) => {
+	console.log(req.body);
+
+	var sql = 'insert into cart set ?';
+
+	var cart_item = {
+		CartId: cart_Id,
+		Quantity: 1,
+		productid: req.body.id,
+	};
+	connection.query(sql, cart_item, (err, rows) => {
 		if (err) {
 			console.log(err);
 		} else {
-			const test = JSON.stringify(rows);
-			res.json(JSON.parse(test));
+			res.json('New Item Added Sucessfully');
+			console.log(rows);
+		}
+	});
+});
+
+//Increase the quantity of an existing cart item by one
+app.post('/add_item_to_cart', (req, res) => {
+	console.log(req.body);
+
+	var sql =
+		'update cart set quantity=quantity+1 where cartId=? and productId=?';
+
+	var cart_item = [cart_Id, req.body.id];
+	connection.query(sql, cart_item, (err, rows) => {
+		if (err) {
+			console.log(err);
+		} else {
+			res.json('Item quantity increased by 1 Sucessfully');
+			console.log(rows);
+		}
+	});
+});
+
+//Decrease the quantity of an existing cart item by one
+app.post('/remove_item_from_cart', (req, res) => {
+	console.log(req.body);
+
+	var sql =
+		'update cart set quantity=quantity-1 where cartId=? and productId=?';
+
+	var cart_item = [cart_Id, req.body.id];
+	connection.query(sql, cart_item, (err, rows) => {
+		if (err) {
+			console.log(err);
+		} else {
+			res.json('Item quantity decreases by 1 Sucessfully');
+			console.log(rows);
+		}
+	});
+});
+
+//Remove the a particular cart item
+app.post('/clear_item_from_cart', (req, res) => {
+	console.log(req.body);
+
+	var sql = 'delete from cart where cartId=? and productId=?';
+
+	var cart_item = [cart_Id, req.body.Id];
+	connection.query(sql, cart_item, (err, rows) => {
+		if (err) {
+			console.log(err);
+		} else {
+			res.json('Item removed Sucessfully');
 			console.log(rows);
 		}
 	});
@@ -135,3 +221,6 @@ app.post('/add_item_to_cart', (req, res) => {
 app.listen(4000, () => {
 	console.log('app is running on port 4000');
 });
+
+// 	var sql =
+// 		"use ecommerce;SET SQL_SAFE_UPDATES = 0;set @productId='3';set @cartId = '1';create table temp (productId varchar(20),cartId varchar(20));Insert into temp VALUES(@productId, @cartId);UPDATE cart set Quantity =Quantity+1 where productId = @productId and cartId=@cartId;insert into cart(productId, CartId, Quantity)select productId, cartId, 1 as'Quantity' from temp where productId not in (select productId from cart where cartId=@cartId);Drop table temp;";
